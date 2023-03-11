01d586a6942a171f8047f60dc823b3c1632cbebb
jjb-dev <84111581+jjbuell-dev@users.noreply.github.com>
2023-03-11 12:59:15 -0700

    Complete Chapter 3 Challenges

--- WorldTrotter-BNR/ConversionViewController.swift
+++ WorldTrotter-BNR/ConversionViewController.swift
@@ -12,7 +12,16 @@ import UIKit
 
 class ConversionViewController: UIViewController {
 
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Chapter 3 Silver Challenge - Add Gradient Layer
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.blue.cgColor, UIColor.green.cgColor, UIColor.yellow.cgColor, UIColor.orange.cgColor, UIColor.red.cgColor, UIColor.purple.cgColor]
+        gradientLayer.frame = view.bounds
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
