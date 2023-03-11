278f00786ee2642d6c6abc6f9ec5601ff59f35cd
jjb-dev <84111581+jjbuell-dev@users.noreply.github.com>
2023-03-11 12:41:16 -0700

    Initial Commit

---
+++ WorldTrotter-BNR/ConversionViewController.swift
@@ -0,0 +1,18 @@
+//
+//
+// WorldTrotter-BNR
+// 
+// ConversionViewController.swift
+//
+// Created by Royal Blue Software
+// 
+
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
